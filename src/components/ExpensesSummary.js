4ef436a325834e50720da2747b634bd902f54de1
ildar <ildar@jepeddo.com>
2018-02-15 12:01:28 -0700

    Updated last commit

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import selectExpenses from '../selectors/expenses';
-import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = (props) => (
-    <div>
-    {
-        props.expenses.length !== 0 && ( 
-            <p>Viewing {props.expenses.length} expenses totaling {numeral(getExpensesTotal(props.expenses)/100).format('$0,0.00')}</p> 
-        )
-    }
-    </div>
-);
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
+    return (
+        <div>
+        {
+            expenseCount !== 0 && ( 
+                <h1 >Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1> 
+            )
+        }
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
     return {
-        
-        expenses: visibleExpenses     
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
     }
 }
 
